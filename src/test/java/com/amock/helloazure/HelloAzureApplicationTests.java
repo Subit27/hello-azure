d3bc479a419e01289bc1c483864e4f1c5657cf28
Subit27 <57525293+Subit27@users.noreply.github.com>
2019-11-15 16:11:48 +0530

    Update HelloAzureApplicationTests.java

--- src/test/java/com/amock/helloazure/HelloAzureApplicationTests.java
+++ src/test/java/com/amock/helloazure/HelloAzureApplicationTests.java
@@ -6,7 +6,9 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
-SpringBootTest
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@SpringBootTest
 public class HelloAzureApplicationTests {
 
 	@Test
